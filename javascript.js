ae9ec1cf2f2a480bc6214ddb9f5784c95a6e854f
Andy Bui <at.bui89@gmail.com>
2022-06-22 14:48:14 -0400

    Added math functions

--- javascript.js
+++ javascript.js
@@ -1,54 +1,149 @@
-function addNums (num1, num2) {
-  return num1 + num2;
+var storedNum1 = '';
+var storedNum2 = '';
+var storedOperation1 = '';
+
+function addNums () {
+  return '+';
 }
 
-function subtractNums (num1, num2) {
-  return num1 - num2;
+function subtractNums () {
+  return '-';
 }
 
-function multiplyNums (num1, num2) {
-  return num1 * num2;
+function multiplyNums () {
+  return '*';
 }
 
-function divideNums (num1, num2) {
-  return num1 / num2;
+function divideNums () {
+  return '÷';
 }
 
-function operate (mathFunc) {
-  var numOne = Number(document.getElementById('num1').value);
-  var numTwo = Number(document.getElementById('num2').value);
-  console.log('Number one : ' + numOne);
-  console.log('Number two : ' + numTwo);
-  var addNums = numOne + numTwo;
- 
-  var addedNums = window[mathFunc](numOne, numTwo);
-
-  console.log('Result = ' + addedNums);
-
-}
-
-const subtract = function(num1, num2) {
-	return num1 - num2;
-};
-
-const sum = function(testArray) {
-	return testArray.reduce((partialSum, a) => partialSum + a, 0);
-};
-
-const multiply = function(testArray) {
-  return testArray.reduce((partialMultiply, a) => partialMultiply * a, 1);
-};
-
-const power = function(num1, num2) {
-	return Math.pow(num1, num2);
-};
-
-const factorial = function(num) {
-  if (num === 0) return 1;
-  let factFn = 1;
-  for (let i = num; i > 0; i--) {
-    factFn *= i;
+function operate(mathFunc) {
+  if (storedNum1 == '') {
+    //nothing
+  } else if (storedOperation1 == '') {
+    storedOperation1 = window[mathFunc]();
+    var targetDiv = document.getElementById('outputDisplay');
+    targetDiv.innerHTML += storedOperation1;
+    console.log('StoredOperation = ' + storedOperation1);
+  } else {
+    runCalc();
+    storedOperation1 = window[mathFunc]();
+    var targetDiv = document.getElementById('outputDisplay');
+    targetDiv.innerHTML = storedNum1 + storedOperation1;
+    console.log('StoredOperation = ' + storedOperation1);
   }
-  return factFn;
-};
+}
 
+function appendNums(numClicked) {
+  if (storedOperation1 == '') {
+    clearDisplay();
+    const numPicked = document.getElementById(numClicked).textContent;
+    console.log('NumPicked = ' + numPicked);
+    var targetDiv = document.getElementById('outputDisplay');
+    storedNum1 += numPicked;
+    targetDiv.innerHTML = storedNum1;
+    console.log('storedNum1 = ' + storedNum1);
+  } else {
+    const numPicked = document.getElementById(numClicked).textContent;
+    console.log('NumPicked = ' + numPicked);
+    var targetDiv = document.getElementById('outputDisplay');
+    targetDiv.innerHTML += numPicked;
+    storedNum2 += numPicked;
+    console.log('storedNum2 = ' + storedNum2);
+  }
+}
+
+function appendDecimal(decClicked) {
+  if (storedOperation1 == '') {
+    clearDisplay();
+    const numPicked = document.getElementById(decClicked).textContent;
+    console.log('NumPicked = ' + numPicked);
+    var targetDiv = document.getElementById('outputDisplay');
+    storedNum1 += numPicked;
+    targetDiv.innerHTML = storedNum1;
+    console.log('storedNum1 = ' + storedNum1);
+  } else {
+    const numPicked = document.getElementById(decClicked).textContent;
+    console.log('NumPicked = ' + numPicked);
+    var targetDiv = document.getElementById('outputDisplay');
+    targetDiv.innerHTML += numPicked;
+    storedNum2 += numPicked;
+    console.log('storedNum2 = ' + storedNum2);
+  }
+}
+
+function equalSign() {
+  runCalc();
+  var targetDiv = document.getElementById('outputDisplay');
+  targetDiv.innerHTML = answer;
+  console.log('The Answer is ' + answer);
+  clearNums();
+}
+
+function runCalc() {
+  if (storedOperation1 == '+') {
+    answer = Number(storedNum1) + Number(storedNum2);
+  } else if (storedOperation1 == '-') {
+    answer = Number(storedNum1) - Number(storedNum2);
+  } else if (storedOperation1 == '*') {
+    answer = Number(storedNum1) * Number(storedNum2);
+  } else if (storedOperation1 == '÷') {
+    answer = Number(storedNum1) / Number(storedNum2);
+  }
+  storedNum1 = answer;
+  storedNum2 = '';
+  console.log('runCalc ran, answer = ' + answer + 'storedNum1 = ' + storedNum1);
+}
+
+function clearAll() {
+  clearNums();
+  clearDisplay();
+  console.log('Cleared');
+  console.log('storedNum1 = ' + storedNum1);
+  console.log('storedNum2 = ' + storedNum2);
+  console.log('StoredOperation = ' + storedOperation1);
+}
+
+function clearNums() {
+  storedNum1 = '';
+  storedNum2 = '';
+  storedOperation1 = '';
+  answer = '';
+  console.log('clearNums ran');
+}
+
+function clearDisplay() {
+  var targetDiv = document.getElementById('outputDisplay');
+  targetDiv.innerHTML = '';
+}
+
+function appendDecimal() {
+  if (storedOperation1 == '') {
+    clearDisplay();
+    const numPicked = document.getElementById('addDecimal').textContent;
+    if (storedNum1.includes('.')) {
+      console.log('NumPicked = ' + numPicked);
+      var targetDiv = document.getElementById('outputDisplay');
+      targetDiv.innerHTML = storedNum1;
+      console.log('storedNum1 = ' + storedNum1);
+    } else {
+    console.log('NumPicked = ' + numPicked);
+    var targetDiv = document.getElementById('outputDisplay');
+    storedNum1 += numPicked;
+    targetDiv.innerHTML = storedNum1;
+    console.log('storedNum1 = ' + storedNum1);
+  }
+  } else {
+    const numPicked = document.getElementById('addDecimal').textContent;
+    if (storedNum2.includes('.')) {
+    console.log('storedNum2 = ' + storedNum2);
+    } else {
+    console.log('NumPicked = ' + numPicked);
+    var targetDiv = document.getElementById('outputDisplay');
+    targetDiv.innerHTML += numPicked;
+    storedNum2 += numPicked;
+    console.log('storedNum2 = ' + storedNum2);
+    }
+  }
+}
